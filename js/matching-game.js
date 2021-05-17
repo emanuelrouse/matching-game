387aebf6b981bcd5c0fea8238a5c4ea6bf60e063
Emanuel Rouse <emanuelrouse@hotmail.com>
2021-05-17 19:26:05 -0400

    Task 2 Generate Faces complete

--- js/matching-game.js
+++ js/matching-game.js
@@ -1,3 +1,24 @@
 'use strict'
 
-console.log('file linked');
+// Global Variables
+let numberOfFaces = 5;
+const theLeftSide = document.querySelector('#left-side');
+
+// Event Listeners
+window.addEventListener("load", generateFaces);
+
+function generateFaces() {
+    // generate faces using a for loop
+    for (let i = 0; i < numberOfFaces; i++) {
+        const face = document.createElement("img");
+        const randomTop = Math.floor(Math.random() * 400) + 1;
+        const randomLeft = Math.floor(Math.random() * 400) + 1;
+
+        face.src = ("images/smile.png");
+        face.style.top = randomTop + 'px';
+        face.style.left = randomLeft + 'px';
+
+        theLeftSide.appendChild(face);
+    }
+}
+
